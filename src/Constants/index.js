493c6c2113a8084596732574e9b96e2d89b1f7c6
Palak <Palakgoyal0304@gmail.com>
2023-04-20 14:04:59 +0530

    Changed the view on mobile of all-suits page

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -51,99 +51,113 @@ export const suitCards = [
 export const allSuits = [
     {
         key: 1,
-        name: "lorem ipsum",
+        name: "BLUEBELLS",
         img: blueSuit,
+        shortD: "Delicate floral blue beauty draped gracefully.",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 2,
-        name: "lorem ipsum",
+        name: "GREENY",
         img: brownJutti,
+        shortD: "Floral Punjabi jutti with green elegance",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 3,
-        name: "lorem ipsum",
+        name: "ELEGANCE",
         img: darkGreen,
+        shortD: "Dark green cloth adorned with yellow flowered Fulkari.",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 4,
-        name: "lorem ipsum",
+        name: "WAVY-ONE",
         img: darkGreenCloth,
+        shortD: "Beachy polka dots on sea-green flora",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 5,
-        name: "lorem ipsum",
+        name: "CLASSY",
         img: grayCloth,
+        shortD: "Rows of square flowers adorn brown",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 6,
-        name: "lorem ipsum",
+        name: "LEAFY",
         img: greenSuit,
+        shortD: "Leafy suit with Fulkari garden",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 7,
-        name: "lorem ipsum",
+        name: "WILLOW",
         img: greenSuitModel,
+        shortD: "Handpainted flower suit on girl",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 8,
-        name: "lorem ipsum",
+        name: "PLUM",
         img: lightPurple,
+        shortD: "Purple suit, Fulkari, pink-yellow florals",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 9,
-        name: "lorem ipsum",
+        name: "ROBIN",
         img: lightBlue,
+        shortD: "Sky-blue casual suit with pink flora",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 10,
-        name: "lorem ipsum",
+        name: "TANGERINE",
         img: orange,
+        shortD: "Graceful orange cloth with light blue flowers",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 11,
-        name: "lorem ipsum",
+        name: "ROSIE",
         img: pinkCloth,
+        shortD: "Light pink suit with red flowers",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 12,
-        name: "lorem ipsum",
+        name: "RAVEN",
         img: purpleSuit,
+        shortD: "Purple party wear suit for next-level beauty",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 13,
-        name: "lorem ipsum",
+        name: "BERRY",
         img: red,
+        shortD: "Stylish red cloth with colorful flowers",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 14,
-        name: "lorem ipsum",
+        name: "MARIGOLD",
         img: redJutti,
+        shortD: "Hand-painted red jutti suit set",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
@@ -151,35 +165,39 @@ export const allSuits = [
         key: 15,
         name: "lorem ipsum",
         img: redSuit,
+        shortD: "Red colored Suit set with Phulkari",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 16,
-        name: "lorem ipsum",
+        name: "LUNA",
         img: whiteCloth,
+        shortD: "Moon like cloth for a moon",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 17,
-        name: "lorem ipsum",
+        name: "MEADOW",
         img: yellow,
+        shortD: "Party wear yellow suit",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
     {
         key: 18,
-        name: "lorem ipsum",
+        name: "DAFFODIL",
         img: yellowSuit,
+        shortD: "Party wear yellow suit",
         price: '5$',
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
     },
-    {
-        key: 19,
-        name: "lorem ipsum",
-        img: yellowSuitM,
-        price: '5$',
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
-    },
+    // {
+    //     key: 19,
+    //     name: "lorem ipsum",
+    //     img: yellowSuitM,
+    //     price: '5$',
+    //     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, ullam. Laborum consequuntur est voluptate explicabo at commodi facilis. Unde ipsam consequatur odio culpa similique in quos esse aspernatur eaque facere"
+    // },
 ]
--- src/components/AllSuitCom.jsx
+++ src/components/AllSuitCom.jsx
@@ -3,17 +3,23 @@ import { allSuits } from '../Constants'
 
 const AllSuits = () => {
   return (
-    <div className='flex flex-wrap gap-3 m-6 justify-evenly items-center'>
-      {allSuits.map((suit) => {
-        return(
-          <div className=''>
-            <img src={ suit.img } alt="" className='w-[90vw] md:w-[44vw] h-96 rounded-2xl border-[1px]' />
-            <div className=''>
-                <h3>{suit.name}</h3>
+    <div className='mx-2'>
+      <div className='my-2'>
+        <h1 className='font-sans font-bold text-xl'>Suits</h1>
+        <p className='text-[10px] text-[#565959]'>Prices and other details may vary according to your customization</p>
+      </div>
+      <div className='grid grid-cols-2 gap-2 justify-evenly items-center'>
+        {allSuits.map((suit) => {
+          return (
+            <div className=' rounded-t-md'>
+              <img src={suit.img} alt="" className='w-[90vw] h-40 md:w-[44vw] md:h-96 rounded-t-md border-[1px]' />
+              <div className='border-[1px] text-center px-2 py-4'>
+                <h3 className='font-medium text-[#0F1111] text-sm'>{suit.name}</h3>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </div>
   )
 }
