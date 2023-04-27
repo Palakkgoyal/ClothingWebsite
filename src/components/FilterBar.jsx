9b2e4104c44a9195899fee03b32fb3e0aca76954
Palak <Palakgoyal0304@gmail.com>
2023-04-27 13:08:04 +0530

    Resolved valtio state management error

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { AiFillCaretDown } from "react-icons/ai";
-import { toggleFilterPage } from '../store';
+import { toggleFilterPage, state } from '../store';
+import { useSnapshot } from 'valtio';
 
 const FilterBar = () => {
+    const snap = useSnapshot( state )
+    const hideOrNot = snap.filterPage? "block" : "hidden";
     return (
-        <div>
+        <div className={``}>
             <div className='lg:hidden flex justify-between px-4 py-4 border-b-2'>
                 <h3 className='text-lg sm:pl-6 md:pl-10'>KiratFlorals</h3>
                 <div className='flex text-blue-700 cursor-pointer hover:filterShadow pl-5 sm:pl-14 border-l-2 sm:pr-6 md:pr-10 justify-self-end'>
--- src/components/FilterCom.jsx
+++ src/components/FilterCom.jsx
@@ -1,72 +1,89 @@
 import React from 'react'
-import { state, toggleFilterPage } from '../store'
-// import { useProxy } from 'valtio/macro'
+import { toggleFilterPage, state } from '../store';
+import { useSnapshot } from 'valtio';
+import { RxCross2 } from "react-icons/rx"
+import { useSearchParams } from 'react-router-dom';
+
 
 const FilterCom = () => {
-    // const snapshot = useProxy(state);
+    const [searchParam, setSearchParam] = useSearchParams()
+    const snap = useSnapshot(state)
+    const hideOrNot = snap.filterPage ? "block" : "hidden";
 
     return (
-        <div className={`lg:hidden`}>
-            {console.log("this component loaded")}
+        <div className={`lg:hidden ${hideOrNot}`}>
             <div className='min-h-full min-w-full z-10 fixed bg-white'>
-                <h3
-                    className='text-base font-bold leading-4 text-[#0F1111] mt-4'>
-                    Colors
-                </h3>
-                <div className='ml-1 mt-2 flex flex-col items-start'>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "red" })}>
-                        Red
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "green" })}>
-                        Green
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "yellow" })}>
-                        Yellow
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "blue" })}>
-                        Blue
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "purple" })}>
-                        Purple
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "orange" })}>
-                        Orange
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "white" })}>
-                        White
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "brown" })}>
-                        Brown
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() => setSearchParam({ color: "pink" })}>
-                        Pink
-                    </button>
-                    <button
-                        className='cursor-pointer hover:underline my-3'
-                        onClick={() =>
-                            setSearchParam({})}>
-                        Clear Filter
-                    </button>
+
+                <div className='flex justify-end'>
+                    <div
+                        className='flex justify-center items-center mt-7 mr-5 text-3xl rounded-full hover:bg-slate-300 w-14 h-14'
+                        onClick={toggleFilterPage}>
+                        <RxCross2 />
+                    </div>
                 </div>
-            <button onClick={ toggleFilterPage }>Close this page</button>
+
+                <div className='ml-8'>
+                    <h3
+                        className='text-lg font-bold leading-4 text-[#0F1111] mt-4'>
+                        Colors
+                    </h3>
+                    <div className=''>
+                        <div className='mt-2 grid grid-cols-2 max-w-[340px]'>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "red" })}>
+                                Red
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "green" })}>
+                                Green
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "yellow" })}>
+                                Yellow
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "blue" })}>
+                                Blue
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "purple" })}>
+                                Purple
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "orange" })}>
+                                Orange
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "white" })}>
+                                White
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "brown" })}>
+                                Brown
+                            </button>
+                            <button
+                                className='cursor-pointer hover:underline my-3 flex justify-start'
+                                onClick={() => setSearchParam({ color: "pink" })}>
+                                Pink
+                            </button>
+                            {/* <button
+                                className='cursor-pointer hover:underline my-3'
+                                onClick={() =>
+                                    setSearchParam({})}>
+                                Clear Filter
+                            </button> */}
+                        </div>
+                    </div>
+                </div>
+                {/* <button onClick={toggleFilterPage}>Close this page</button> */}
             </div>
         </div>
     )
